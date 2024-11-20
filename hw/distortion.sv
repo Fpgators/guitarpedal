dfbef0dbd3cfc5d13b90bd69f3d3b43784b60ef2
Danton Klein <uf.fpgators@gmail.com>
2024-11-19 20:24:22 -0500

    first draft of distortion.sv

--- hw/distortion.sv
+++ hw/distortion.sv
@@ -7,11 +7,29 @@ module distortion
 (
     input logic clk,
     input logic reset,
+    input logic enable,
     input logic [WIDTH-1:0] in,
     input logic [WIDTH-1:0] gain,
     input logic [WIDTH-1:0] threshold,
+    input logic [WIDTH-1:0] volume,
     output logic [WIDTH-1:0] out
 
 );
+logic comparator_out;
+logic[WIDTH-1:0] comparator_in;
+logic[WIDTH-1:0] gained_signal;
+logic[WIDTH-1:0] gained_signal_registered;
+logic[WIDTH-1:0] mux_out;
+logic[WIDTH-1:0] rectifier_in;
+logic[WIDTH-1:0] rectifier_out;
+logic[WIDTH-1:0] threshold_signal_input;
+
+
+register #(.WIDTH(WIDTH)) multiplier_register (input(gained_signal), output(gained_signal_registered), .#);
+rectifier #(.WIDTH(WIDTH)) signal_rectifier (input(rectifier_in), output(rectifier_out));
+register #(.WIDTH(WIDTH)) rectifier_register (input(rectifier_out), output(comparator_in), .#);
+comparator #(.WIDTH(WIDTH)) threshold_comparator (in0(comparator_in), in1(threshold), out(comparator_out));
+register #(.WIDTH(WIDTH)) threshold_mux_in_register (input(gained_signal_registered), output(threshold_signal_input), .#);
+multiplexer #(.WIDTH(WIDTH)) threshold_mux (.in0(threshold_signal_input), .in1(threshold), sel(comparator_out), .out(mux_out));
 
 endmodule
--- hw/primitives.sv
+++ hw/primitives.sv
@@ -1,1 +1,97 @@
-// This file contains multiple modules that will have to be used a lot, such as a register, multiplexer, multiplier, adder, comparator, etc.
+// This file contains multiple modules that will have to be used a lot, such as a register, multiplexer, multiplier, adder, comparator, rectifier.
+
+module register
+#(
+    parameter WIDTH
+)
+(
+    input logic clk,
+    input logic reset,
+    input logic enable,
+    input logic [WIDTH-1:0] in,
+    output logic [WIDTH-1:0] out
+);
+
+always_ff @(posedge clk or posedge rst) begin
+    //If reset is 1, set output to 0
+    if(reset)
+        out <= '0;
+    //If enable is 1, set output to input
+    else if (enable)
+        out <= in;
+
+end
+
+endmodule
+
+module multiplexer
+#(
+    parameter WIDTH
+    
+)
+(
+    input logic [WIDTH-1:0] in0,
+    input logic [WIDTH-1:0] in1,
+    input logic sel,
+    output logic [WIDTH-1:0]out
+);
+
+//If select is 1 bring in1 to otput, otherwise bring in0
+assign out = sel == 1'b1 ? in1 : in0;
+
+endmodule
+
+module multiplier
+#(
+    parameter WIDTH
+)
+(
+    input logic[WIDTH-1:0] a,
+    input logic[WIDTH-1:0] b,
+    input logic[2*WIDTH-1:0] product
+);
+
+assign product = signed'a * signed'b; 
+
+endmodule
+
+module adder
+#(
+    parameter WIDTH
+)
+(
+    input logic [WIDTH-1:0] in0, in1,
+    input logic carry_in,
+    output logic [WIDTH-1:0] sum,
+    output logic carry_out,
+);
+
+assign {carry_out, sum} = in0 + in1 + carry_in;
+
+endmodule
+
+module comparator
+#(
+    parameter WIDTH
+)
+(
+    input logic [WIDTH-1:0] in0, in1,
+    output logic out,
+);
+
+    assign out = (in0 >= in1);
+
+endmodule
+
+module rectifier
+#(
+    parameter WIDTH
+)
+(
+    input logic [WIDTH-1:0] input,
+    output logic [WIDTH-1:0] output,
+);
+// Checks if last bit is 1, flips input sign if true.
+    assign output = input[WIDTH-1] ? ~input + 1 : input;
+
+endmodule
